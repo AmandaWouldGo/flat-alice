73f059d1ce864555480a8588a6429e5236f32fd6
AmandaWouldGo <amandawouldgo@gmail.com>
2016-08-27 15:58:32 -0700

    Solve canvas drawing issue and add self invoking function to script

--- application.js
+++ application.js
@@ -1,14 +1,31 @@
-function startGame() {
-	gamePlaySpace.start(); // startGame is a function that invokes the start function on the play space
-}
+(function () {
 
-var gamePlaySpace = {
-	canvas : document.createElement("canvas"),
-	// start makes the canvas
-	start : function() {
-		this.canvas.width = 960;
-		this.canvas.height = 540;
-		this.context = this.canvas.getContext("2d");
-		document.body.insertBefore(this.canvas, document.body.childnodes[0])
+	var myGamePiece;
+
+	function startGame() {
+	    myGameArea.start();
+	    myGamePiece = new component(30, 30, "red", 10, 120);
 	}
-}
+
+	var myGameArea = {
+	    canvas : document.createElement("canvas"),
+	    start : function() {
+	        this.canvas.width = 960;
+	        this.canvas.height = 540;
+	        this.context = this.canvas.getContext("2d");
+	        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+	    }
+	}
+
+	function component(width, height, color, x, y) {
+	    this.width = width;
+	    this.height = height;
+	    this.x = x;
+	    this.y = y;
+	    ctx = myGameArea.context;
+	    ctx.fillStyle = color;
+	    ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+
+	document.addEventListener("onload", startGame());
+} ());
