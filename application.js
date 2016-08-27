30838d40a6f715a148ac64d5248133efa8fa8a3e
AmandaWouldGo <amandawouldgo@gmail.com>
2016-08-27 16:08:08 -0700

    Animate square to move across canvas

--- application.js
+++ application.js
@@ -4,7 +4,7 @@
 
 	function startGame() {
 	    myGameArea.start();
-	    myGamePiece = new component(30, 30, "red", 10, 120);
+	    myGamePiece = new component(30, 30, "#63b8ff", 10, 120);
 	}
 
 	var myGameArea = {
@@ -14,6 +14,10 @@
 	        this.canvas.height = 540;
 	        this.context = this.canvas.getContext("2d");
 	        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+	        this.interval = setInterval(updateGameArea, 20);
+	    },
+	    clear : function() {
+	    	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	    }
 	}
 
@@ -22,9 +26,17 @@
 	    this.height = height;
 	    this.x = x;
 	    this.y = y;
-	    ctx = myGameArea.context;
-	    ctx.fillStyle = color;
-	    ctx.fillRect(this.x, this.y, this.width, this.height);
+	    this.update = function() {
+		    ctx = myGameArea.context;
+		    ctx.fillStyle = color;
+		    ctx.fillRect(this.x, this.y, this.width, this.height);
+		}
+	}
+
+	function updateGameArea() {
+		myGameArea.clear();
+		myGamePiece.x += 1;
+		myGamePiece.update();
 	}
 
 	document.addEventListener("onload", startGame());
