0431023611789c6b22b12d1608fc14dd777dfc40
AmandaWouldGo <amandawouldgo@gmail.com>
2016-08-27 17:22:55 -0700

    Make aliceGamePiece move with arrow keys

--- application.js
+++ application.js
@@ -17,6 +17,13 @@
 	        this.context = this.canvas.getContext("2d");
 	        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 	        this.interval = setInterval(updateGameArea, 20);
+	        window.addEventListener('keydown', function (e) {
+	        	myGameArea.keys = (myGameArea.keys || []);
+	        	myGameArea.keys[e.keyCode] = (e.type == "keydown");
+	        })
+	        window.addEventListener('keyup', function (e) {
+	        	myGameArea.keys[e.keyCode] = (e.type == "keydown");
+	        })
 	    },
 	    clear : function() {
 	    	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -26,6 +33,8 @@
 	function component(width, height, color, x, y) {
 	    this.width = width;
 	    this.height = height;
+	    this.speedX = 0;
+	    this.speedY = 0
 	    this.x = x;
 	    this.y = y;
 	    this.update = function() {
@@ -33,17 +42,30 @@
 		    ctx.fillStyle = color;
 		    ctx.fillRect(this.x, this.y, this.width, this.height);
 		}
+		this.newPos = function() {
+			this.x += this.speedX;
+			this.y += this.speedY;
+		}
 	}
 
 	function updateGameArea() {
 		myGameArea.clear();
-		aliceGamePiece.x += 1;
-		queenGamePiece.y += 1;
-		caterGamePiece.x += 1;
-		caterGamePiece.y -= 1;
+		// queenGamePiece.y += 1;
+		// caterGamePiece.x += 1;
+		// caterGamePiece.y -= 1;
+		// queenGamePiece.update();
+		// caterGamePiece.update();
+
+		aliceGamePiece.speedX = 0;
+		aliceGamePiece.speedY = 0;
+		if (myGameArea.keys && myGameArea.keys[37]) { aliceGamePiece.speedX = -1; }
+		if (myGameArea.keys && myGameArea.keys[39]) { aliceGamePiece.speedX = 1; }
+		if (myGameArea.keys && myGameArea.keys[38]) { aliceGamePiece.speedY = -1; }
+		if (myGameArea.keys && myGameArea.keys[40]) { aliceGamePiece.speedY = 1; }
+
+		
+		aliceGamePiece.newPos();
 		aliceGamePiece.update();
-		queenGamePiece.update();
-		caterGamePiece.update();
 	}
 
 	document.addEventListener("onload", startGame());
