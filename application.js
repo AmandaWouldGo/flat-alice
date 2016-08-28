9d950c25021c0c554da32db10e09d55ad7337328
AmandaWouldGo <amandawouldgo@gmail.com>
2016-08-27 19:57:28 -0700

    Add crashWith and stop functions so that the main game piece has an obstacle

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 (function () {
 
-	var aliceGamePiece, queenGamePiece;
+	var aliceGamePiece;
+	var queenGamePiece = [];
 
 	function startGame() {
 	    myGameArea.start();
@@ -16,6 +17,7 @@
 	        this.canvas.height = 540;
 	        this.context = this.canvas.getContext("2d");
 	        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+	        this.frameNo = 0;
 	        this.interval = setInterval(updateGameArea, 20);
 	        window.addEventListener('keydown', function (e) {
 	        	myGameArea.keys = (myGameArea.keys || []);
@@ -27,7 +29,15 @@
 	    },
 	    clear : function() {
 	    	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	    },
+	    stop : function() {
+	    	clearInterval(this.interval);
 	    }
+	}
+
+	function everyinterval(n) {
+		if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    	return false;
 	}
 
 	function component(width, height, color, x, y) {
@@ -46,26 +56,47 @@
 			this.x += this.speedX;
 			this.y += this.speedY;
 		}
+		this.crashWith = function(otherobj) {
+			var myleft = this.x;
+			var myright = this.x + (this.width);
+			var mytop = this.y;
+			var mybottom = this.y + (this.height);
+			var otherleft = otherobj.x;
+			var otherright = otherobj.x + (otherobj.width);
+			var othertop = otherobj.y;
+			var otherbottom = otherobj.y + (otherobj.height);
+			var crash = true;
+			if ((mybottom < othertop) ||
+					(mytop > otherbottom) ||
+					(myright < otherleft) ||
+					(myleft > otherright)) {
+				crash = false;
+			}
+			return crash;
+		}
 	}
 
 	function updateGameArea() {
-		myGameArea.clear();
-		// queenGamePiece.y += 1;
-		// caterGamePiece.x += 1;
-		// caterGamePiece.y -= 1;
-		// queenGamePiece.update();
-		// caterGamePiece.update();
+		if (aliceGamePiece.crashWith(queenGamePiece)) {
+			myGameArea.stop();
+		} else {	
+			myGameArea.clear();
+			queenGamePiece.y += 1;
+			caterGamePiece.x += 1;
+			caterGamePiece.y -= 1;
+			aliceGamePiece.speedX = 0;
+			aliceGamePiece.speedY = 0;
 
-		aliceGamePiece.speedX = 0;
-		aliceGamePiece.speedY = 0;
-		if (myGameArea.keys && myGameArea.keys[37]) { aliceGamePiece.speedX = -1; }
-		if (myGameArea.keys && myGameArea.keys[39]) { aliceGamePiece.speedX = 1; }
-		if (myGameArea.keys && myGameArea.keys[38]) { aliceGamePiece.speedY = -1; }
-		if (myGameArea.keys && myGameArea.keys[40]) { aliceGamePiece.speedY = 1; }
-
-		
-		aliceGamePiece.newPos();
-		aliceGamePiece.update();
+			if (myGameArea.keys && myGameArea.keys[37]) { aliceGamePiece.speedX = -1; }
+			if (myGameArea.keys && myGameArea.keys[39]) { aliceGamePiece.speedX = 1; }
+			if (myGameArea.keys && myGameArea.keys[38]) { aliceGamePiece.speedY = -1; }
+			if (myGameArea.keys && myGameArea.keys[40]) { aliceGamePiece.speedY = 1; }
+			
+			aliceGamePiece.newPos();
+			aliceGamePiece.update();
+			queenGamePiece.update();
+			caterGamePiece.update();
+		}
 	}
 
 	document.addEventListener("onload", startGame());
